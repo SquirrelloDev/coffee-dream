64f432318fd1f00a83ef8eee776ef1378f198fdc
Emil Olejnik <olejnike2000@gmail.com>
2022-12-28 14:25:50 +0100

    Added scaffoling to bottom bar

---
+++ client/src/components/navs/BottomBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import classes from "./BottomBar.module.css";
+class BottomBar extends Component{
+    render() {
+        return(
+            <div className={classes['bottom-bar']}>
+
+            </div>
+        )
+    }
+}
+export default BottomBar;

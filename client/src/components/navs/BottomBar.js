ff104db6c488ab60c809176d821f76aa871c9504
Emil Olejnik <olejnike2000@gmail.com>
2022-12-28 16:26:26 +0100

    Created layout for bottom bar and added FontAwesome icons

--- client/src/components/navs/BottomBar.js
+++ client/src/components/navs/BottomBar.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import classes from "./BottomBar.module.scss";
+import NavBtn from "./NavBtn";
 class BottomBar extends Component{
     render() {
         return(
             <div className={classes['bottom-bar']}>
-
+                <NavBtn icon="home"/>
+                <NavBtn icon="cart"/>
+                <NavBtn icon="user"/>
             </div>
         )
     }
---
+++ client/src/components/navs/NavBtn.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHouse} from "@fortawesome/free-solid-svg-icons";
+import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import classes from "./BottomBar.module.scss";
+const icons = {
+    home: faHouse,
+    cart: faCartShopping,
+    user: faUser
+}
+class NavBtn extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {desiredIcon: null}
+    }
+    componentDidMount() {
+            if(Object.keys(icons).includes(this.props.icon)){
+               console.log("Exist!");
+               this.setState({desiredIcon: icons[`${this.props.icon}`]})
+            }
+            else{
+                console.error("No icon found!");
+            }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <button className={classes.icon__btn}><FontAwesomeIcon icon={this.state.desiredIcon} size="xl" className={classes['icon']}/></button>
+            </div>
+        );
+    }
+}
+export default NavBtn;

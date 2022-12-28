da47db45186a56772533deb61151b9f210f2e0ef
Maccer <maciekp1797@gmail.com>
2022-12-28 12:59:48 +0100

    added basic server.js code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,39 @@
-//Masisz's playground
+require(`dotenv`).config({path: `./config/.env`})
+
+require(`./config/db`)
+
+const express = require(`express`)
+const app = express()
+
+app.use(require(`body-parser`).json())
+app.use(require(`cors`)({credentials: true, origin: process.env.LOCAL_HOST}))
+
+app.listen(process.env.SERVER_PORT, () => 
+{
+    console.log(`Connected to port ` + process.env.SERVER_PORT)
+})
+
+app.use((req, res, next) => {next(createError(404))})
+
+app.use(function (err, req, res, next)
+{       
+    if (!err.statusCode) 
+    {
+        err.statusCode = 500
+    }
+    
+    // check that all required paramters are not empty in any route
+    if (err instanceof ReferenceError)
+    {
+        err.statusCode = 400
+        err.message = "Cannot reference a variable that has not been declared. This can be caused in run-time if the user did not input a parameter that is required by a router"
+    }
+    
+    // Server-side error message
+    console.log(err.message + "\nError Details...")
+    // Server-side error details
+    console.log(err)
+    
+    // return error message that will be displayed on client-side console
+    res.status(err.statusCode).send(err.message)    
+})

c490de2aa09618bc8511f030fbe1541947f19861
Maccer <maciekp1797@gmail.com>
2022-12-28 12:44:55 +0100

    added users model

---
+++ server/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema(
+{
+  login: { type: String, required: true, unique: true},
+  email: { type: String, required: true, unique: true, validate: {
+      validator: function(value) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
+      },
+      message: 'Invalid email format'
+    }
+  },
+  password: { type: String, required: true, minlength: 8 },
+  accessLevel: {type: Number, default:parseInt(process.env.ACCESS_LEVEL_NORMAL_USER)},
+},
+{
+    collection: `users`
+});
+
+module.exports = mongoose.model('users', usersSchema);

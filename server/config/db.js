8cf880f19bf40a7dd2a9f6e74cbd9be02441e441
Maccer <maciekp1797@gmail.com>
2022-12-28 13:50:39 +0100

    removed deprecated code

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true})
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))

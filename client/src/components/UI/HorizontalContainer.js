4617cf24b95abf1352bb222ba1783a1c10295072
Emil Olejnik <olejnike2000@gmail.com>
2022-12-30 21:07:16 +0100

    Fixed link styles on card

--- client/src/components/UI/HorizontalContainer.js
+++ client/src/components/UI/HorizontalContainer.js
@@ -2,12 +2,13 @@ import React from "react";
 import Card from "./Card";
 import classes from "./HorizontalContainer.module.scss";
 import ProductCard from "../Products/ProductCard";
+import {Link} from "react-router-dom";
 
 class HorizontalContainer extends React.Component{
     render() {
         return (
             <div className={`${classes.container} ${classes['snaps-inline']}`}>
-                <ProductCard stockValue={20}>Arabica with nut</ProductCard>
+                <Link to='/product/1' className={classes.link}><ProductCard stockValue={20}>Arabica with nut</ProductCard></Link>
                 <ProductCard stockValue={20}>Test with 11</ProductCard>
                 <ProductCard stockValue={20}>Test with 11</ProductCard>
                 <ProductCard stockValue={20}>Test with 11</ProductCard>

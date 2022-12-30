2314dcf3ac8522916cb3eddd22a5b2497739f5a7
Emil Olejnik <olejnike2000@gmail.com>
2022-12-30 18:34:09 +0100

    Added quantity component

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -4,6 +4,11 @@ import testImg from "../img/coffebag2.png";
 import testImg2 from '../img/coffebag1.png';
 import classes from "./ProductPage.module.scss";
 import SCA from "../components/Products/SCA";
+import QuantityBox from "../components/Products/QuantityBox";
+import Button from "../components/UI/Button";
+import{faBagShopping} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 class ProductPage extends React.Component{
     render() {
         return (
@@ -12,11 +17,18 @@ class ProductPage extends React.Component{
                 <div className={classes['image-box']}>
                     <img src={testImg2}/>
                 </div>
-                <section className={classes.main__box}>
+                <section className={`${classes.main__box} ${classes.section}`}>
                     <p className={classes.main__box__title}>Arabica </p>
                     <SCA/>
                     <p className={classes.main__box__price}><span>$</span>29.99</p>
                     <p className={classes.main__box__stock}>In stock: 216</p>
+                </section>
+                <section>
+                {/*buttony dla admina */}
+                </section>
+                <section className={`${classes.section} ${classes['action-box']}`}>
+                    <QuantityBox labelEnabled={true}/>
+                    <Button variant='fill' disabled={false}>TO CART <FontAwesomeIcon icon={faBagShopping}/></Button>
                 </section>
             </main>
         );

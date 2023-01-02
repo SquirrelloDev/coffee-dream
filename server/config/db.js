2173aef276ba72cda31838361ae55791170a669a
Maccer <maciekp1797@gmail.com>
2023-01-02 12:17:59 +0100

    added db auto increment config

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false);
 mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
+autoIncrement.initialize(mongoose.connection)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))

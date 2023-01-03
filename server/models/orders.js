496eedefca242a3be1003620dba9e9b41e9eb20a
Maccer <maciekp1797@gmail.com>
2023-01-03 13:18:49 +0100

    updated orders model

--- server/models/orders.js
+++ server/models/orders.js
@@ -3,13 +3,15 @@ const autoIncrement = require('mongoose-auto-increment');
 
 const OrdersSchema = new mongoose.Schema({
     orderId: { type: String, unique: true, required: true},
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
     products: [{
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Products', required: true },
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Products', required: true },
       quantity: { type: Number, required: true } }],
     total: { type: Number, required: true },
     status: { type: String, enum: ['pending', 'shipped', 'delivered', 'cancelled'], default: 'pending' },
-    shippingAddress: { type: String, required: true}
+    address: { type: String, required: true},
+    city: { type: String, required: true},
+    zipcode: { type: String, required: true}
     },
     {
     collection: `Orders`

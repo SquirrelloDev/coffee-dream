ed0647f44d890e94cd4bb01832c9ad1e6799db2b
Emil Olejnik <olejnike2000@gmail.com>
2023-01-06 11:46:55 +0100

    Added global constant file

---
+++ client/src/config/global_const.js
@@ -0,0 +1,3 @@
+export const ACCESS_LEVEL = {GUEST: 0, USER: 1, ADMIN: 2}
+export const PAYPAL_SANDBOX = 'AX1wDeFyiK7d8_RdQwGfsaDGDZWQWeEq8YdEaCqhkTl9AfIS1aAZMNBijCjqAVysEAoKTWHh4JjO4t75';
+export const SERVER_PATH = 'http://localhost:4000';
